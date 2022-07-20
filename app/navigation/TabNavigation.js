265c7249e073d14a56d5615840f55b920c2e99ae
Jose20796 <jose_brice_bas1996@outlook.com>
2022-07-19 20:07:36 -0500

    Taller 1: Clase 13
    Agregar 4 tabs al proyecto

---
+++ app/navigation/TabNavigation.js
@@ -0,0 +1,83 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function SearchScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Buscar!</Text>
+    </View>
+  );
+}
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Perfil!</Text>
+    </View>
+  );
+}
+
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigation() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "ios-home" : "ios-home-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "ios-settings" : "ios-settings-outline";
+          }
+          switch (route.name) {
+            case "Home":
+              iconName = focused ? "ios-home" : "ios-home-outline";
+              break;
+            case "Settings":
+              iconName = focused ? "ios-settings" : "ios-settings-outline";
+              break;
+            case "Search":
+              iconName = focused ? "ios-search" : "ios-search-outline";
+              break;
+            case "Profile":
+              iconName = focused ? "ios-person" : "ios-person-outline";
+              break;
+
+            default:
+              break;
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "green",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
