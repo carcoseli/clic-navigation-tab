31272e51de584987947738287bbae60f258799bd
hllulluna <102400539+hllulluna@users.noreply.github.com>
2022-08-06 08:23:34 -0500

    Registro de usuario
    Autenticación con Firebase

---
+++ app/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-lJOwevWDkSy9UNRlkO9bCYxBW8JUauw",
+  authDomain: "clic-app-3e0d8.firebaseapp.com",
+  projectId: "clic-app-3e0d8",
+  storageBucket: "clic-app-3e0d8.appspot.com",
+  messagingSenderId: "709321168846",
+  appId: "1:709321168846:web:9c4a5249b96f6695473d9f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const authentication = getAuth(app);
