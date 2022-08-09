8a9f4647d68562ba9bbae731401db69cc619ef5f
Jose20796 <jose_brice_bas1996@outlook.com>
2022-08-08 23:31:42 -0500

    Tarea1 Clase 15: Inicio de sesion
    Tarea1 Clase 15: Inicio de sesión de CLIP APP

---
+++ app/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyAlUBtgXaTpK6erlJzLlj8JG4HYmcWTRYI",
+  authDomain: "clic-app-2aa5b.firebaseapp.com",
+  projectId: "clic-app-2aa5b",
+  storageBucket: "clic-app-2aa5b.appspot.com",
+  messagingSenderId: "1063272133683",
+  appId: "1:1063272133683:web:a41fe24a9d08715f611f46"
+};
+
+const app = initializeApp(firebaseConfig);
+export const authentication =getAuth(app);
